880fcd3ee793005e2728a30fe352d51ec11dc44b
ma-nobrega <matheus.adao.nobrega@gmail.com>
2022-08-23 20:07:35 -0300

    add routes in the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
-import { Button } from "./components/Button/index";
 import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonVariant, Container } from "./styles";
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <Container variant={variant}>Enviar</Container>;
-}
