c9a4e031fe6dad9874e3b3514cf0afd30e8f4659
ma-nobrega <matheus.adao.nobrega@gmail.com>
2022-09-07 16:32:40 -0300

    cycle completed

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -78,7 +78,7 @@ export const Separator = styled.div`
   justify-content: center;
 `;
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -86,18 +86,29 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${({ theme }) => theme["gray-100"]};
+
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-  background: ${({ theme }) => theme["green-500"]};
-  color: ${({ theme }) => theme["gray-100"]};
-
-  &:not(:disabled):hover {
-    background: ${({ theme }) => theme["green-700"]};
-  }
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
+  }
+`;
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${({ theme }) => theme["green-500"]};
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme["green-700"]};
+  }
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${({ theme }) => theme["red-500"]};
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme["red-700"]};
   }
 `;
