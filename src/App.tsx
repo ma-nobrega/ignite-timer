3ee5cc77835c5b094b80d4dbc9e0d6ff68c116f5
ma-nobrega <matheus.adao.nobrega@gmail.com>
2022-09-10 12:57:20 -0300

    add cycles context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { GlobalStyle } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
